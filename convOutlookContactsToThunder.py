644a8014de36c50b3b2e020201df86bfb36e0b36
Sherman Chen <sherman.chen08@gmail.com>
2023-07-16 15:39:26 -0400

    Made maximum line length 78.

--- convOutlookContactsToThunder.py
+++ convOutlookContactsToThunder.py
@@ -19,22 +19,39 @@ def importContacts():
     Returns:
         df2 (dataFrame): contacts of Outlook
     """
-    df = pd.read_csv(INFILE, encoding='utf_8', low_memory=False) # If there is encoding problem, use detectEncoding.py to find the encoding, and substitute it in this line.
+    df = pd.read_csv(INFILE, encoding='utf_8', low_memory=False)
+    # If there is encoding problem, use detectEncoding.py to find the
+    # encoding, and substitute it in this line.
     # df = pd.read_csv(INFILE, encoding='Windows-1252', low_memory=False)
-    df2 = df.loc[:, ['First Name','Last Name','Company','Department','Job Title','Business Street','Business City','Business State','Business Postal Code','Business Fax','Business Phone','Business Phone 2','Home Phone','Mobile Phone','E-mail Address','E-mail 2 Address','E-mail 3 Address','Notes']]
+    df2 = df.loc[:, ['First Name', 'Last Name', 'Company', 'Department',
+            'Job Title', 'Business Street', 'Business City', 'Business State',
+            'Business Postal Code', 'Business Fax', 'Business Phone',
+            'Business Phone 2', 'Home Phone', 'Mobile Phone', 'E-mail Address',
+            'E-mail 2 Address', 'E-mail 3 Address', 'Notes']]
     return df2
 
 
 def createEmptyThundDF():
-    """ Create an empty dataframe which has all the columns of Thunderbird contact csv. Add one row with the value NaN for every column.
+    """ Create an empty dataframe which has all the columns of Thunderbird
+    contact csv. Add one row with the value NaN for every column.
 
     Parameters:
         None
 
     Returns:
-        dfo (dataFrame): a one-row dataframe which has all the columns of Thunderbird contact csv.
+        dfo (dataFrame): a one-row dataframe which has all the columns of
+        Thunderbird contact csv.
     """
-    thundColNames = ['First Name', 'Last Name', 'Display Name', 'Nickname', 'Primary Email', 'Secondary Email', 'Screen Name', 'Work Phone', 'Home Phone', 'Fax Number', 'Pager Number', 'Mobile Number', 'Home Address', 'Home Address 2', 'Home City', 'Home State', 'Home Postal Code', 'Home Country', 'Work Address', 'Work Address 2', 'Work City', 'Work State', 'Work Postal Code', 'Work Country', 'Job Title', 'Department', 'Organization', 'Web Page 1', 'Web Page 2', 'Birth Year', 'Birth Month', 'Birth Day', 'Custom 1', 'Custom 2', 'Custom 3', 'Custom 4', 'Notes']
+    thundColNames = ['First Name', 'Last Name', 'Display Name', 'Nickname',
+            'Primary Email', 'Secondary Email', 'Screen Name', 'Work Phone',
+            'Home Phone', 'Fax Number', 'Pager Number', 'Mobile Number',
+            'Home Address', 'Home Address 2', 'Home City', 'Home State',
+            'Home Postal Code', 'Home Country', 'Work Address',
+            'Work Address 2', 'Work City', 'Work State', 'Work Postal Code',
+            'Work Country', 'Job Title', 'Department', 'Organization',
+            'Web Page 1', 'Web Page 2', 'Birth Year', 'Birth Month',
+            'Birth Day', 'Custom 1', 'Custom 2', 'Custom 3', 'Custom 4',
+            'Notes']
     dfo = pd.DataFrame(columns = thundColNames)
     numCol = len(dfo.columns)
     rowOfNaN = [np.nan] * numCol
@@ -43,14 +60,16 @@ def createEmptyThundDF():
 
 
 def convert(df2):
-    """ Change Outlook contact column names into Thunderbird contact column names.
+    """ Change Outlook contact column names into Thunderbird contact column
+    names.
 
     Parameters:
         df2 (dataFrame): contacts of Outlook
 
     Returns:
-        df2 (dataFrame): Outlook contacts in which column names have been changed into Thunderbird contact column names.
-        """
+        df2 (dataFrame): Outlook contacts in which column names have been
+        changed into Thunderbird contact column names.
+    """
     columnMapping = {
             'E-mail Address': 'Primary Email',
             'E-mail 2 Address': 'Secondary Email',
@@ -69,15 +88,27 @@ def convert(df2):
 
 
 def produceOutDF(df2, dfo):
-    """ Concatenate Outlook df with Thunderbird one-row df. Change the order of column names as per the column name order of Thunderbird contacts.
+    """ Concatenate Outlook df with Thunderbird one-row df. Change the order
+    of column names as per the column name order of Thunderbird contacts.
 
     Parameters:
-        df2 (dataFrame): Outlook contacts in which column names have been changed into Thunderbirdcontact column names.
+        df2 (dataFrame): Outlook contacts in which column names have been
+        changed into Thunderbirdcontact column names.
     Returns:
-        dfo3 (dataFrame): produced Thunderbird contact dataframe. The order of the column names has been changed as per Thunderbird contacts.
+        dfo3 (dataFrame): produced Thunderbird contact dataframe. The order of
+        the column names has been changed as per Thunderbird contacts.
     """
     dfo2 = pd.concat([df2, dfo]) # After concat, index of last row is '0'
-    new_order = ['First Name', 'Last Name', 'Display Name', 'Nickname', 'Primary Email', 'Secondary Email', 'Screen Name', 'Work Phone', 'Home Phone', 'Fax Number', 'Pager Number', 'Mobile Number', 'Home Address', 'Home Address 2', 'Home City', 'Home State', 'Home Postal Code', 'Home Country', 'Work Address', 'Work Address 2', 'Work City', 'Work State', 'Work Postal Code', 'Work Country', 'Job Title', 'Department', 'Organization', 'Web Page 1', 'Web Page 2', 'Birth Year', 'Birth Month', 'Birth Day', 'Custom 1', 'Custom 2', 'Custom 3', 'Custom 4', 'Notes']
+    new_order = ['First Name', 'Last Name', 'Display Name', 'Nickname',
+            'Primary Email', 'Secondary Email', 'Screen Name', 'Work Phone',
+            'Home Phone', 'Fax Number', 'Pager Number', 'Mobile Number',
+            'Home Address', 'Home Address 2', 'Home City', 'Home State',
+            'Home Postal Code', 'Home Country', 'Work Address',
+            'Work Address 2', 'Work City', 'Work State', 'Work Postal Code',
+            'Work Country', 'Job Title', 'Department', 'Organization',
+            'Web Page 1', 'Web Page 2', 'Birth Year', 'Birth Month',
+            'Birth Day', 'Custom 1', 'Custom 2', 'Custom 3', 'Custom 4',
+            'Notes']
     dfo3 = dfo2.loc[:, new_order]
     return dfo3
 
@@ -86,7 +117,8 @@ def exportContacts(dfo3):
     """ Export the produced contacts dataframe into a csv file.
 
     Parameters:
-        dfo3 (dataFrame): produced Thunderbird contact dataframe. The order of the column names has been changed as per Thunderbird contacts.
+        dfo3 (dataFrame): produced Thunderbird contact dataframe. The order of
+        the column names has been changed as per Thunderbird contacts.
 
     Returns:
         None
@@ -94,14 +126,10 @@ def exportContacts(dfo3):
     dfo3.to_csv(OUTFILE, index=False)
 
 
-def main():
+if __name__ == '__main__':
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
     df2 = importContacts()
     dfo = createEmptyThundDF()
     df2 = convert(df2)
     dfo3 = produceOutDF(df2, dfo)
     exportContacts(dfo3)
-
-
-if __name__ == '__main__':
-    pd.set_option("display.max_rows", None, "display.max_columns", None)
-    main()
